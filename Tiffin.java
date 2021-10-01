652c53b67074b6606f89259b65a1578f5a1da087
Tiffin <tfilion@rpa.com>
2021-09-30 17:44:28 -0700

    fixing another error

--- Tiffin.java
+++ Tiffin.java
@@ -1,7 +1,7 @@
 public class Tiffin {
     public static void main(String[] args) {
         
-        System.out.println(("I am initializing this file.");
+        System.out.println("I am initializing this file.");
 
     }
 }
