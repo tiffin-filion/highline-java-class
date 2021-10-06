b65b0d744e03483863a1c4947688346f3aa2fd09
Tiffin <tfilion@rpa.com>
2021-10-06 12:07:38 -0700

    Completed problems 7 and 8

--- revelChapterTwo/Section02_07.java
+++ revelChapterTwo/Section02_07.java
@@ -1,13 +1,29 @@
 package revelChapterTwo;
 
+import java.util.Scanner;
+
 public class Section02_07 {
 
 	public static void main(String[] args) {
-		/* Write a program that prompts the user to enter the minutes
-		 * and displays the number of years and days for the minutes
-		 * For simplicity, assume a year has 365 days
-		 */
+		// Program converts minutes into years and days (assuming year is not a leap year)
 
+		 // enable Scanner so user can send input to pro389gram
+		 Scanner input = new Scanner(System.in);
+
+		 // prompt user to enter minutes
+		 System.out.print("Enter an amount of minutes to get the amount of years and days: ");
+		 int minutes = input.nextInt();
+
+		 // get number hours
+		 int hours = minutes / 60;
+
+		 // get number of days
+		 int days = hours / 24;
+
+		 // get remainging days after finding years
+		 int years = days / 365;
+		 days = days % 365;
+		 System.out.println(minutes + " minutes is " + years + " year(s) & " + days + " days.");
 	}
 
 }
--- revelChapterTwo/Section02_08.java
+++ revelChapterTwo/Section02_08.java
@@ -1,5 +1,7 @@
 package revelChapterTwo;
 
+import java.util.Scanner;
+
 public class Section02_08 {
 
 	public static void main(String[] args) {
@@ -10,21 +12,32 @@ public class Section02_08 {
 		 * the specified time zone.
 		 */
 		
+		// add Scanner so user can enter time zone offset
+		Scanner input = new Scanner(System.in);
+
+		// ask user for timezone offset
+		System.out.println("Include the - or + sign.");
+		System.out.println("Enter a timezone offset to convert GMT time to your local timezone: ");
+		System.out.println("Example: PST is -7");
+		long offset = input.nextLong();
+
+		long offsetSeconds = offset * 3600;
+
 		// Obtain the total milliseconds since midnight, Jan 1, 1970
 		long totalMilliseconds = System.currentTimeMillis();
 		
 		// Obtain the total seconds  since midnight, Jan 1, 1970
-		long totalSeconds = totalMilliseconds / 1000;
-		
-		// Computer the current second in the minute in the hour
+		long totalSeconds = (totalMilliseconds / 1000) + offsetSeconds;
+
+		// Compute the current second in the minute in the hour
 		long currentSecond = totalSeconds % 60;
 		
 		// Obtain the total minutes
 		long totalMinutes = totalSeconds / 60;
 		
-		// Compute the current minute in the hour
-		long currentMinute = totalMinutes % 60;
-		
+		// Compute the current minute in the hour and convert using offset
+		long currentMinute = (totalMinutes % 60);
+
 		// Obtain the total hours
 		long totalHours = totalMinutes / 60;
 		
@@ -32,8 +45,7 @@ public class Section02_08 {
 		long currentHour = totalHours % 24;
 		
 		// Display results
-		System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond + "GMT");
-
+		System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);
 	}
 
 }
