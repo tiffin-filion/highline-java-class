dc898420db0664a7adbd6b06bdfbe1f7ed3fcfd7
Tiffin Filion <tfilion@rpa.com>
2021-10-19 17:51:04 -0700

    add project file

---
+++ revelChapterThree/Exercise03_23.java
@@ -0,0 +1,28 @@
+package revelChapterThree;
+
+import java.util.Scanner;
+
+public class Exercise03_23 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // user enters a point
+        System.out.print("Enter a point with two coordinates: ");
+        System.out.print("As # # (2 2)");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+
+        // rectangle with center as 0 0
+
+        // Point (x, y) is (not) in the rectangle
+        if (((x <= 5.0) && (x >= -5.0)) && ((y <= 2.5) && (y >= -2.5))) {
+            System.out.println("Point (" + x + ", " + y + ") is in the rectangle");
+        } else {
+            System.out.println("Point (" + x + ", " + y + ") is not in the rectangle");
+        }
+
+    }
+
+}
